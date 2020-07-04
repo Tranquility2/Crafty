64e73115d2986a38e14da29da6ab74150a2aa7c9
Roy Moore <roy@moore.co.il>
2020-07-04 17:19:44 +0300

    add test time icon

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-time_icon_url = 'https://wiki.factorio.com/images/Time.png'
+time_icon_url = 'https://wiki.factorio.com/images/Time_icon.png'
 
-icon_height = 32 // px
-icon_width = 32 // px
+icon_height = "32" // px
+icon_width = "32" // px
 
 function build() {
     var inputs = parseInt(document.getElementById("inputs").value)
     var outputs = parseInt(document.getElementById("outputs").value)
     var add_time = document.getElementById("add_time").checked 
     console.log("inputs=%d outputs=%d add_time=%s", inputs, outputs, add_time)
+
+    if (add_time == true) {
+        var element = document.getElementById("build_area");
+        var para = document.createElement("div");
+        para.className = "factorio-icon";
+        para.style.backgroundColor = "#999";
+
+        sub_para = document.createElement("IMG");
+        sub_para.src = time_icon_url;
+        sub_para.height = icon_height;
+        sub_para.width = icon_width;
+        para.appendChild(sub_para);
+
+        text_para = document.createElement("div");
+        text_para.className = "factorio-icon-text"
+        text_para.innerHTML = "10" // time
+        para.appendChild(text_para);
+
+        element.appendChild(para);
+    }
+    
 }
 
