2070e5d05a827f358287f39062d0e161467c5f7e
Roy Moore <roy@moore.co.il>
2020-07-10 19:30:04 +0300

    new adding method

--- js/script.js
+++ js/script.js
@@ -4,68 +4,52 @@ time_icon_url = 'https://wiki.factorio.com/images/Time_icon.png'
 icon_height = "32"
 icon_width = "32"
 
+var IconType = {
+    Input: 'input',
+    Output: 'output',
+};
+
 build_area = document.getElementById("build_area");
+Reset();
 
-
-function BuildIconElement(icon_url="", count="") {
-    var div_elem = document.createElement("div");
-    div_elem.className = "factorio-icon";
-    div_elem.style.backgroundColor = "#999";
+function BuildIconElement(icon_url, count, icon_type) {
+    console.log("Adding %s Icon (count=%s, url=%s)", icon_type, count, icon_url)
+    var icon_elem = document.createElement("div");
+    icon_elem.className = "factorio-icon";
+    icon_elem.style.backgroundColor = "#999";
+    icon_elem.onclick = function() {alert('t')};
 
     if (icon_url) {
         img_elem = document.createElement("IMG");
         img_elem.src = icon_url;
         img_elem.height = icon_height;
         img_elem.width = icon_width;
-        div_elem.appendChild(img_elem);
-
-        text_elem = document.createElement("div");
-        text_elem.className = "factorio-icon-text"
-        text_elem.innerHTML = count // time
-        div_elem.appendChild(text_elem);
+        icon_elem.appendChild(img_elem);
     }
 
-    return div_elem
+    text_elem = document.createElement("div");
+    text_elem.className = "factorio-icon-text"
+    text_elem.innerHTML = count
+    icon_elem.appendChild(text_elem);
+
+    return icon_elem
 }
 
-function AddEmptyIcon() {
-    build_area.appendChild(BuildIconElement());
-    console.log("Added empty icon");
-}
-
-function CleanBuildArea() {
+function Reset() {
     build_area.innerHTML =""
+    build_area.appendChild(BuildIconElement(icon_url="", count="1", icon_type=IconType.Input));
+    build_area.innerHTML += "→";
+    build_area.appendChild(BuildIconElement(icon_url="", count="1", icon_type=IconType.Output));
 }
 
-function CreateBuild() {
-    var inputs = parseInt(document.getElementById("inputs").value)
-    var outputs = parseInt(document.getElementById("outputs").value)
-    var add_time = document.getElementById("add_time").checked 
-    console.log("inputs=%d outputs=%d add_time=%s", inputs, outputs, add_time)
-    // Do we need to cleanup
-    if (build_area.innerHTML) {
-        CleanBuildArea()
-    }
-    // Add time only if requested
-    if (add_time) {
-        build_area.appendChild(BuildIconElement(time_icon_url, "10"));
+function AddIcon(icon_type) {
+    if (icon_type==IconType.Input) {
+        build_area.innerHTML = "+" + build_area.innerHTML
+        build_area.prepend(BuildIconElement(icon_url="", count="1", icon_type=IconType.Input));
+    } else if (icon_type==IconType.Output) {
         build_area.innerHTML += "+";
+        build_area.appendChild(BuildIconElement(icon_url="", count="1", icon_type=IconType.Output));
+    } else {
+        console.log("%s is not a valid option", icon_type)
     }
-    
-    for (i = 0; i < inputs; i++) {
-        build_area.appendChild(BuildIconElement());
-        if (i < inputs - 1) {
-            build_area.innerHTML += "+";
-        }
-    }
-
-    build_area.innerHTML += "→";
-
-    for (i = 0; i < outputs; i++) {
-        build_area.appendChild(BuildIconElement());
-        if (i < outputs - 1) {
-            build_area.innerHTML += "+";
-        }
-    }
-    
 }
